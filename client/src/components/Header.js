b494b882bd14305db8c53bbc65ac934d216c29af
Erosas15 <rosaserick15@ucla.edu>
2024-07-06 17:31:19 -0700

    initated the header
    added style to the header
    added functionality for page routing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,49 @@
+import logo from "./photos/logo.png";
+import "../styles/header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+
     return (
-        <div> 
-            header
+        <div className="header">
+            <div className="header-color"/>
+
+            <div className="header-top"> 
+                <div className="header-left">
+                    <div className="logo">
+                        <img src={logo} alt="LOGO"/>
+                    </div>
+
+                    <div className="company-name">Comfort Care 24/7 </div>
+                </div>
+
+                <div className="header-right">
+                    <div className="subtext">Get Pricing & Schedule Tour</div>
+                    <div className="phone-number">951-500-0904</div>
+                </div>            
+            </div>
+
+            <div className="nav-header">
+                <Link to="/about-us">
+                    <button className="nav-item-header">About Us</button>
+                </Link>
+
+                <Link to="/independent-living">
+                    <button className="nav-item-header">Independent Living</button>
+                </Link>
+
+                <Link to="/partnership">
+                    <button className="nav-item-header">Partnership</button>
+                </Link>
+
+                <Link to="/contact-us">
+                    <button className="nav-item-header">Contact Us</button>
+                </Link>
+                
+            </div>
+
+            <div className="header-color"/>
+
         </div>
     )
 }
