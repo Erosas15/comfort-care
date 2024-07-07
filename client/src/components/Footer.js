4e9522b58888f31da9ba25bc91d335974ab61f47
Erosas15 <rosaserick15@ucla.edu>
2024-07-06 17:35:14 -0700

    initated the footer
    added style
    added page functionality

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,38 @@
+import "../styles/footer.css";
+import { Link } from "react-router-dom";
+
 
 const Footer = () => {
+
     return (
-        <div>
-            footer
+        <div className="footer">
+            <div className="footer-contents">
+
+                <div className="footer-color"/>
+
+
+                <div className="nav-footer">
+                    <Link to="/about-us">
+                        <button className="nav-item-footer">About Us</button>
+                    </Link>
+
+                    <Link to="/independent-living">
+                        <button className="nav-item-footer">Independent Living</button>
+                    </Link>
+
+                    <Link to="/partnership">
+                        <button className="nav-item-footer">Partnership</button>
+                    </Link>
+
+                    <Link to="/contact-us">
+                        <button className="nav-item-footer">Contact Us</button>
+                    </Link>
+                </div>
+            </div>
+
+            <div className ="copyright-notice">@Comfort Care 24/7 LLC</div>
+            <div className="footer-color"/>
+
         </div>
     )
 }
